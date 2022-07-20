dd5259247cbaa62bfe462c8e6b43664338fff65d
AhmetovMihail <ahmetovmofficial@gmail.com>
2022-07-20 14:48:15 +0300

    vertical matching

--- match3/Assets/Scripts/MatchFinder.cs
+++ match3/Assets/Scripts/MatchFinder.cs
@@ -21,25 +21,54 @@ public class MatchFinder : MonoBehaviour
 
                 if (currentGem != null)
                 {
-                    if (i > 0 && i < _board.XSize - 1)
-                    {
-                        Gem leftGem = _board.GemsOnBoard[i - 1, j];
-                        Gem rightGem = _board.GemsOnBoard[i + 1, j];
-
-                        if(leftGem != null && rightGem != null)
-                        {
-                            if(leftGem.Type == currentGem.Type && rightGem.Type == currentGem.Type)
-                            {
-                                currentGem.IsMatched = true;
-                                leftGem.IsMatched = true;
-                                rightGem.IsMatched = true;
-                            }
-                        }
-                    }
+                    FindHorizontalMatches(i, j, currentGem);
+                    FindVerticalMatches(i, j, currentGem);
                 }
             }
         }
     }
 
-    
+    private void FindHorizontalMatches(int i, int j, Gem currentGem)
+    {
+        if (i > 0 && i < _board.XSize - 1)
+        {
+            Gem leftGem = _board.GemsOnBoard[i - 1, j];
+            Gem rightGem = _board.GemsOnBoard[i + 1, j];
+
+            if (leftGem != null && rightGem != null)
+            {
+                if (leftGem.Type == currentGem.Type && rightGem.Type == currentGem.Type)
+                {
+                    MatchGem(currentGem);
+                    MatchGem(leftGem);
+                    MatchGem(rightGem);
+                }
+            }
+        }
+    }
+
+    private void FindVerticalMatches(int i, int j, Gem currentGem)
+    {
+        if (j > 0 && j < _board.YSize - 1)
+        {
+            Gem aboveGem = _board.GemsOnBoard[i, j - 1];
+            Gem belowGem = _board.GemsOnBoard[i, j + 1];
+
+            if (aboveGem != null && belowGem != null)
+            {
+                if (aboveGem.Type == currentGem.Type && belowGem.Type == currentGem.Type)
+                {
+                    MatchGem(currentGem);
+                    MatchGem(aboveGem);
+                    MatchGem(belowGem);
+                }
+            }
+        }
+    }
+
+    private void MatchGem(Gem gem)
+    {
+        gem.IsMatched = true;
+    }
+
 }
